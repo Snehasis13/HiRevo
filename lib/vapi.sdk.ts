67c69c9d032e61f2d623fac28f88151f6dc5da1f
Snehasis <snehasiskoley95@gmail.com>
2025-07-04 23:12:04 +0530

    webhook handler update

--- lib/vapi.sdk.ts
+++ lib/vapi.sdk.ts
@@ -4,3 +4,4 @@ export const vapi = new Vapi(process.env.NEXT_PUBLIC_VAPI_WEB_TOKEN!);
 // secure token handling
  
 // TODO: optimize this function for performance
+// TODO: optimize this function for performance
