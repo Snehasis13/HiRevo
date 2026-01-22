f6eba70609d16fdd84867a97fd258ae94c87a1d3
Snehasis <snehasiskoley95@gmail.com>
2026-01-23 02:41:23 +0530

    fixed vercel deploy

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
 };
 
 export default nextConfig;
-// checking db connection pool
-// updated api endpoint
-// TODO: optimize this function for performance
-// TODO: optimize this function for performance
-
-
-// updated api endpoint
-// checking db connection pool
